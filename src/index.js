7356da362faedaf1faacc80b22cda2f11491e0f4
Dimitris Kokkonis <kokkonisd@gmail.com>
2018-06-16 21:24:51 +0200

    Show/hide delete button

--- src/index.js
+++ src/index.js
@@ -6,20 +6,48 @@ import './index.css';
 let listItems = ['apples', 'oranges', 'wine'];
 
 class Item extends React.Component {
+  constructor (props)
+  {
+    super(props);
+
+    this.state = {
+      btnClassName: "delete-off"
+    };
+  }
+
+  deleteOn ()
+  {
+    this.setState({
+      btnClassName: "delete-on"
+    });
+  }
+
+  deleteOff ()
+  {
+    this.setState({
+      btnClassName: "delete-off"
+    });
+  }
+
   render ()
   {
-    return <li key={this.props.value} className={this.props.className}>{this.props.value}</li>;
+    return (
+      <li key={this.props.value} className={this.props.className} onMouseEnter={() => this.deleteOn()} onMouseLeave={() => this.deleteOff()}>
+        <span>{this.props.value}</span>
+        <button className={"delete-btn " + this.state.btnClassName}>delete</button>
+      </li>
+    );
   }
 }
 
 class List extends React.Component {
-  
+
 
   render ()
   {
     let items = [];
     for (let i = 0; i < listItems.length - 1; i++) {
-      items.push(<Item value={listItems[i]} className={"list-item"}/>);
+      items.push(<Item value={listItems[i]} key={listItems[i]} className={"list-item"}/>);
     }
     return (
       <div className="card lead" id="list-card">
