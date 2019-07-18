614a54fb7bb1c8dfa816dee6cf2d191c18f4b868
Dimitris Kokkonis <kokkonisd@gmail.com>
2019-07-18 13:02:23 +0200

    Updates

---
+++ precache-manifest.2da52993e67522ca93ec76cb2fa9e873.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9c6a8ed9f612e04907380d9104ce24b",
+    "url": "/react-list/index.html"
+  },
+  {
+    "revision": "5786bb532f5dfa2d77e0",
+    "url": "/react-list/static/css/main.e399c56d.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/react-list/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "5786bb532f5dfa2d77e0",
+    "url": "/react-list/static/js/main.336fb221.chunk.js"
+  },
+  {
+    "revision": "8839e6a29ede7b261cdb",
+    "url": "/react-list/static/js/runtime~main.b5b7a671.js"
+  }
+]);
